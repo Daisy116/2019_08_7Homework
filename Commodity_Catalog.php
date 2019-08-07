a40f7d04ed8d419e291b17320ab58631601026a2
Daisy116 <52897966+Daisy116@users.noreply.github.com>
2019-08-07 17:52:04 +0800

    Add files via upload

---
+++ Commodity_Catalog.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div data-role="header" style="text-align: center">
+	    <h1 style="font-family:fantasy;font-size:40px;color:orange">Commodity Catalog</h1>
+    </div>
+
+
+    <div style="position: absolute;top: 75%;left: 50%;margin-right: -50%;transform: translate(-50%, -50%)">
+        <img src="image1.jfif" alt="" style="vertical-align:middle"><a href="Commodity_Detail.php" style="font-size:24px;text-decoration:none;color:black">anello--熱賣商品</a>
+        <br><br>
+        <img src="image2.jfif" alt="" style="vertical-align:middle"><a href="#" style="font-size:24px;text-decoration:none;color:black">時尚百搭多功能防盜背包</a>
+        <br><br>
+        <img src="image3.jfif" alt="" style="vertical-align:middle"><a href="#" style="font-size:24px;text-decoration:none;color:black">多口袋手提後背兩用包</a>
+    </div>
+</body>
+</html>
---
+++ Commodity_Detail.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>商品介紹</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+</head>
+<body>
+    <div data-role="header" style="text-align: center">
+	    <h1 style="font-family:fantasy;font-size:40px;color:purple">Commodity Detail</h1>
+    </div>
+
+    <div class="col-md-3" ></div>
+
+    <div class="col-md-3" >
+        <div style="display: inline-block;">
+            <img src="image1.jfif" alt="">
+        </div>
+        <div style="display: inline-block; tex">
+            anello--熱賣商品
+        </div>
+    </div>
+
+    
+</body>
+</html>
